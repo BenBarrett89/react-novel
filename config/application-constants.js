ff291b2e6ae8a75350c68f27429b32861a59fd9c
Ben Barrett <benjamin.barrett@dwp.gsi.gov.uk>
2016-12-02 19:13:37 +0000

    Some changes

---
+++ config/application-constants.js
@@ -0,0 +1,9 @@
+const PROTOCOL = 'http://'
+const HOST = 'localhost'
+const PORT = 8080
+
+module.exports = {
+  PROTOCOL: PROTOCOL,
+  HOST: HOST,
+  PORT: PORT
+}
---
+++ server/server.js
@@ -0,0 +1,34 @@
+const http = require('http')
+const express = require('express')
+const path = require('path')
+
+const { PORT } = require('../config/application-constants')
+const portNum = process.env.WFMSPORT || PORT
+
+const app = express()
+const server = http.createServer(app)
+
+app.use(express.static(path.resolve(__dirname, '../build/')))
+
+const startServer = (portNum) => {
+  return server.listen(portNum, () => {
+    const { port } = server.address()
+    console.log(`Server listening on port ${port}`)
+    console.log('Press crtl+c to stop the server')
+  })
+}
+
+process.on('SIGINT', () => {
+  server.close()
+  process.exit()
+})
+process.on('exit', () => {
+  server.close()
+  process.exit()
+})
+process.on('uncaughtException', () => {
+  server.close()
+  process.exit()
+})
+
+startServer(portNum)
